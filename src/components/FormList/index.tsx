e148c0bdb96345f4605a129594512a8946108016
Allohamora <herman2000@yandex.ru>
2020-04-30 22:13:35 +0300

    Revert "Removed opportunity append item with empty text"
    This reverts commit 3377970249d111f22bfffcbcc65ccbe96afb5149.

--- src/components/FormList/index.tsx
+++ src/components/FormList/index.tsx
@@ -21,9 +21,6 @@ const FormList = (props: FormListProps) => {
 
     const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
-        if( !value.trim() ) return;
-
         setItems(
             [
                 ...items, 
@@ -48,8 +45,8 @@ const FormList = (props: FormListProps) => {
     }
 
     return (
-        <div className="form-list__wrap" >
-            <form onSubmit={submitHandler} className="form-list" >
+        <>
+            <form onSubmit={submitHandler} >
                 <input
                     type="text"
                     value={value}
@@ -58,7 +55,7 @@ const FormList = (props: FormListProps) => {
                 <button>send</button>
             </form>
             <List items={items} removeHandler={removeHandler} addHandler={addHandler} />
-        </div>
+        </>
     );
 };
 
